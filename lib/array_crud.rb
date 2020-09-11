39ebd35d2e7a97dbb2329482fe507b8677f4bb3d
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-11 21:02:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   t_family = ["Zoe", "Nika", "Eugene", "Alena"]
-  t_family.push("mummy")
+  t_family << ("mummy")
 end
 
 def add_element_to_start_of_array(array, element)
